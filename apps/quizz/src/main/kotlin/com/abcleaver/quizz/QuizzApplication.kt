9f71837d669ef77edf3fe95390908a21063bf804
enzo giammatteo <giammatteoenzo@gmail.com>
2022-03-09 16:11:18 +0000

    Merged in feature/AB-22-créer-ms-quizz (pull request #7)
    feat: Adding quizz service listening in 8084
    
    * feat: Adding quizz service listening in 8084
    
    
    Approved-by: Swan BENOIT

---
+++ apps/quizz/src/main/kotlin/com/abcleaver/quizz/QuizzApplication.kt
@@ -0,0 +1,11 @@
+package com.abcleaver.quizz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizzApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizzApplication>(*args)
+}
