9b078c85264fe72a39e930882aeceeec48722830
swan <swan@argelium.fr>
2022-03-10 11:38:18 +0100

    bugfix: fix statistic

--- apps/statistic/src/main/kotlin/com/abcleaver/statistic/controller/MainController.kt
+++ apps/statistic/src/main/kotlin/com/abcleaver/statistic/controller/MainController.kt
@@ -1,4 +1,4 @@
-package com.abclever.gateway.controller
+package com.abcleaver.statistic.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
