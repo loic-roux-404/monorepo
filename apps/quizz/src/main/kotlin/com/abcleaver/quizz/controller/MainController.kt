807634eb70bef99b5f6b6cf63ab0253cea23ef4b
Rayane Kettana <rayane.kettana@codgency.fr>
2022-03-10 11:52:13 +0100

    feat: Mise en place de kafka sur quizz. réception du message par le consumer ok

--- apps/quizz/src/main/kotlin/com/abcleaver/quizz/controller/MainController.kt
+++ apps/quizz/src/main/kotlin/com/abcleaver/quizz/controller/MainController.kt
@@ -1,12 +1,34 @@
 package com.abcleaver.quizz.controller
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MainController {
+class MainController (@Value("\${kafka.topics.product}") val topic:String,
+                      @Autowired
+                      private val kafkaTemplate: KafkaTemplate<String, Any>){
   @GetMapping("/")
-  fun index(): String {
-    return "blog"
+  fun post(): ResponseEntity<Any>{
+    return try {
+      val message: Message<String> = MessageBuilder.withPayload("Message")
+        .setHeader(KafkaHeaders.TOPIC, topic)
+        .build()
+      kafkaTemplate.send(message)
+      ResponseEntity.ok().build()
+    } catch (e: Exception) {
+      ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error")
+    }
+    return ResponseEntity.ok().build()
+
   }
 }
